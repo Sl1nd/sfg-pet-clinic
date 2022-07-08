0f2164599f8519c6d7c6f4a1b63f6791a74af221
Slindauer <sven.lindauer@inovex.de>
2022-07-08 14:18:46 +0200

    Add owners page & ctrl

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/vets/VetsController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/vets/VetsController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetsController{
 
-  @RequestMapping({"vets"})
+
+  @RequestMapping({"","/", "/index", "/index.html"})
   public String listVets(Model model) {
     return "vets/index";
   }
