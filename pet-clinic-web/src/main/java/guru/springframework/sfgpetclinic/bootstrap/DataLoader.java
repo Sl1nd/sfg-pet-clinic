1f01900a895c2c61912757202c02bbdc4910147a
Slindauer <sven.lindauer@inovex.de>
2022-07-16 10:26:48 +0200

    Autogenerate ids for Map

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     Owner owner1 = new Owner();
-    owner1.setId(1L);
     owner1.setFistName("Michael");
     owner1.setFistName("Weston");
 
     ownerService.save(owner1);
 
     Owner owner2 = new Owner();
-    owner2.setId(2L);
     owner2.setFistName("Fiona");
     owner2.setFistName("Glenner");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
     System.out.println("Loading owners...");
 
     Vet vet1 = new Vet();
-    vet1.setId(1L);
     vet1.setFistName("Sam");
     vet1.setFistName("Axe");
 
    vetService.save(vet1);
 
     Vet vet2 = new Vet();
-    vet2.setId(2L);
     vet2.setFistName("Jessie");
     vet2.setFistName("Porter");
 
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/vets/VetsController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/vets/VetsController.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.controllers.vets;
 
+import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetsController{
 
+  public final VetService vetService;
 
+  public VetsController(VetService vetService) {
+    this.vetService = vetService;
+  }
   @RequestMapping({"","/", "/index", "/index.html"})
   public String listVets(Model model) {
+    model.addAttribute("vets", vetService.findAll());
     return "vets/index";
   }
 }
