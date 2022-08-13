ccfbd60068ba55cebb8e4e0b169c884f91c8ad88
Slindauer <sven.lindauer@inovex.de>
2022-08-13 06:52:59 +0200

    Update base entitiy with JPA annotation

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   public Long getId() {
@@ -13,3 +19,4 @@ public class BaseEntity implements Serializable {
     this.id = id;
   }
 }
+
