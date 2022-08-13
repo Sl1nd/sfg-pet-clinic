78880ee98f1ca85bec92a0e3b864c2d5fea48bce
Slindauer <sven.lindauer@inovex.de>
2022-08-13 07:32:50 +0200

    Add JPA mapping for enities

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -13,6 +15,9 @@ public class Pet extends BaseEntity{
   @JoinColumn(name="owner_id")
   private Owner owner;
   private LocalDate birthday;
+
+ @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+  private Set<Visit> visits = new HashSet<>();
 
   public PetType getPetType() {
     return petType;
@@ -36,5 +41,13 @@ public class Pet extends BaseEntity{
 
   public void setBirthday(LocalDate birthday) {
     this.birthday = birthday;
+  }
+
+  public Set<Visit> getVisits() {
+    return visits;
+  }
+
+  public void setVisits(Set<Visit> visits) {
+    this.visits = visits;
   }
 }
